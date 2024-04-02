092708188d8b54c3722c9cf8566fb9da895b0a37
lostbean <talktoedgar@gmail.com>
2024-04-02 13:02:25 -0300

    add more lang plugins

---
+++ lua/plugins/nix.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    optional = true,
+    opts = {
+      servers = {
+        ["nil_ls"] = {
+          ["nil"] = {},
+        },
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, {
+        "nix",
+      })
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        nix = { "alejandra" },
+      },
+    },
+  },
+  {
+    "LnL7/vim-nix",
+    ft = {
+      "nix",
+    },
+  },
+}
