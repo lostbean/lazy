43f8392972981ac8138faa69e050127cf9c0e1b7
lostbean <talktoedgar@gmail.com>
2024-04-03 11:54:18 -0300

    add multicursor support

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -5,4 +5,22 @@ return {
       manual_mode = false,
     },
   },
+
+  {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "smoka7/hydra.nvim",
+    },
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Multi Cursor",
+      },
+    },
+  },
 }
