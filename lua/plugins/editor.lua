8493eda87f4423ccb33c1cff09e95584ba67bcec
lostbean <talktoedgar@gmail.com>
2024-06-02 23:09:34 -0300

    multicusor more colorful

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -16,8 +16,9 @@ return {
     },
     init = function()
       vim.g.VM_Leader = "\\"
+      vim.g.VM_theme = "neon"
       vim.g.VM_default_mappings = 1
-      vim.g.VM_mouse_mappings = 1
+      vim.g.VM_mouse_mappings = 0
       vim.g.VM_maps = {
         ["Find Under"] = "<C-d>",
         ["Find Subword Under"] = "<C-d>",
