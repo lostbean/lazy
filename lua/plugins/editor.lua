4a0fdb915116524f38e22b3c5cb9714ad5e16f20
lostbean <talktoedgar@gmail.com>
2024-11-22 09:25:44 -0300

    use multicursors.nvim instead

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -7,27 +7,45 @@ return {
   },
 
   {
-    "mg979/vim-visual-multi",
-    lazy = false,
-    keys = {
-      "<C-d>",
-      "<M-C-Down>",
-      "<M-C-Up>",
-      "\\",
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvimtools/hydra.nvim",
     },
-    init = function()
-      vim.g.VM_Leader = "\\"
-      vim.g.VM_theme = "neon"
-      vim.g.VM_default_mappings = 1
-      vim.g.VM_mouse_mappings = 0
-      vim.g.VM_maps = {
-        ["Find Under"] = "<C-d>",
-        ["Find Subword Under"] = "<C-d>",
-        ["Add Cursor Down"] = "<M-C-Down>",
-        ["Add Cursor Up"] = "<M-C-Up>",
-      }
-    end,
+    opts = {},
+    cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
+    keys = {
+      {
+        mode = { "v", "n" },
+        "<Leader>m",
+        "<cmd>MCstart<cr>",
+        desc = "Create a selection for selected text or word under the cursor",
+      },
+    },
   },
+  -- -- Visual Multi seems to be broken
+  -- {
+  --   "mg979/vim-visual-multi",
+  --   lazy = false,
+  --   keys = {
+  --     "<C-d>",
+  --     "<M-C-Down>",
+  --     "<M-C-Up>",
+  --     "\\",
+  --   },
+  --   init = function()
+  --     vim.g.VM_Leader = "\\"
+  --     vim.g.VM_theme = "neon"
+  --     vim.g.VM_default_mappings = 1
+  --     vim.g.VM_mouse_mappings = 0
+  --     vim.g.VM_maps = {
+  --       ["Find Under"] = "<C-d>",
+  --       ["Find Subword Under"] = "<C-d>",
+  --       ["Add Cursor Down"] = "<M-C-Down>",
+  --       ["Add Cursor Up"] = "<M-C-Up>",
+  --     }
+  --   end,
+  -- },
 
   {
     "folke/flash.nvim",
